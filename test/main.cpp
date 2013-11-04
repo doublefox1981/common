ba505bce9e0d0b2b4e553737f298bcf5e5977d48
doublefox1981 <331847809@qq.com>
2013-11-04 10:40:24 +0800

    增加scopeguard，把尽量多的资源管理等交给编译器

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,7 @@
 #include "../net/connection.h"
 #include "../net/netpack.h"
 #include "../base/memorystream.h"
+#include "../base/scopeguard.h"
 
 #include <limits>
 using namespace net;
@@ -11,6 +12,7 @@ using namespace std;
 int main()
 {
 	InitNetwork();
+
 	ezEventLoop* ev1=new ezEventLoop;
 	ev1->init(new ezSelectPoller(ev1),new ezServerHander((numeric_limits<uint16_t>::max)()));
 	ev1->serveOnPort(10010);
@@ -20,6 +22,8 @@ int main()
 	ev->init(new ezSelectPoller(ev),new ezClientHander((numeric_limits<uint16_t>::max)()));
 	ev->getConnectionMgr()->connectTo(ev,"127.0.0.1",10010);
 	Sleep(1000);
+
+	base::ScopeGuard guard([&](){delete ev1; delete ev;});
 
 	while(true)
 	{
