7e3e6fdb12c5562091e79d24993f235ec8fdfa36
doublefox1981 <331847809@qq.com>
2013-12-06 16:45:44 +0800

    1

--- net/netpack.cpp
+++ net/netpack.cpp
@@ -1,28 +1,153 @@
 #include "netpack.h"
 #include "buffer.h"
 #include <string.h>
+using namespace net;
+using namespace base;
 
-net::ezNetPack::ezNetPack(uint16_t s)
+void net::ezMsgInit(ezMsg* msg)
 {
-	assert(s<(ezBuffer::ezInitSize-2));
-	capacity_=s;
-	size_=0;
-	data_=new char[s];
+  msg->type_=type_vsm;
+  msg->size_=0;
+  msg->flags_=0;
 }
 
-net::ezNetPack::~ezNetPack()
+void net::ezMsgInitSize(ezMsg* msg,int size)
 {
-	if(data_)
-		delete[] data_;
+  if (size<=max_vsm_size) 
+  {
+    msg->type_=type_vsm;
+    msg->flags_=0;
+    msg->size_=(uint16_t)size;
+  }
+  else if(size<std::numeric_limits<uint16_t>::max())
+  {
+    msg->type_=type_lmsg;
+    msg->flags_=0;
+    msg->size_=0;
+    msg->heap_.ptr_=(ezMsg::ezBigMsg*)malloc(sizeof(ezMsg::ezBigMsg)+size);
+    msg->heap_.ptr_->data_=(int8_t*)(msg->heap_.ptr_+1);
+    msg->heap_.ptr_->capcity_=size;
+    msg->heap_.ptr_->ffn_=nullptr;
+    msg->heap_.ptr_->hint_=nullptr;
+    new(&msg->heap_.ptr_->refcnt_) base::AtomicNumber();
+  }
+  else {assert(false);}
 }
 
-
-net::ezSendBlock::ezSendBlock():fd_(-1),pack_(nullptr)
+void net::ezMsgInitData(ezMsg* msg,int8_t* data,int size,msg_free_fn* ffn,void* hint)
 {
+  msg->type_=type_lmsg;
+  msg->flags_=0;
+  msg->size_=size;
+  msg->heap_.ptr_=(ezMsg::ezBigMsg*)malloc(sizeof(ezMsg::ezBigMsg));
+  msg->heap_.ptr_->data_=data;
+  msg->heap_.ptr_->capcity_=size;
+  msg->heap_.ptr_->ffn_=ffn;
+  msg->heap_.ptr_->hint_=hint;
+  new(&msg->heap_.ptr_->refcnt_) base::AtomicNumber();
 }
 
-net::ezSendBlock::~ezSendBlock()
+void net::ezMsgInitDelimiter(ezMsg* msg)
 {
-	if(pack_)
-		delete pack_;
+  msg->type_=type_delimiter;
+  msg->flags_=0;
+  msg->size_=0;
+}
+
+int  net::ezMsgSize(ezMsg* msg)
+{
+  return msg->size_;
+}
+
+int net::ezMsgCapcity(ezMsg* msg)
+{
+  switch(msg->type_)
+  {
+  case type_vsm:
+    return max_vsm_size;
+  case type_lmsg:
+    return msg->heap_.ptr_->capcity_;
+  default:
+    return 0;
+  }
+}
+
+int8_t* net::ezMsgData(ezMsg* msg)
+{
+  switch(msg->type_)
+  {
+  case type_vsm:
+    return msg->stack_.data_;
+  case type_lmsg:
+    return msg->heap_.ptr_->data_;
+  default:
+    return nullptr;
+  }
+}
+
+void net::ezMsgAddRef(ezMsg* msg,int ref)
+{
+  if(ref<=0)
+    return;
+  if(msg->type_==type_lmsg)
+  {
+    if(msg->flags_&shared)
+      msg->heap_.ptr_->refcnt_.Add(ref);
+    else
+    {
+      msg->heap_.ptr_->refcnt_.Add(ref+1);
+      msg->flags_|=shared;
+    }
+  }
+}
+
+void net::ezMsgSubRef(ezMsg* msg,int ref)
+{
+  if(ref<=0)
+    return;
+  if(msg->type_!=type_lmsg||!(msg->flags_&shared))
+  {
+    ezMsgFree(msg);
+    return;
+  }
+  ezMsg::ezBigMsg* big=msg->heap_.ptr_;
+  if(!big->refcnt_.Sub(ref)) 
+  {
+    big->refcnt_.~AtomicNumber();
+    if(big->ffn_)
+      big->ffn_(big->data_,big->hint_);
+    free (big);
+  }
+}
+
+void net::ezMsgFree(ezMsg* msg)
+{
+  if(msg->type_==type_lmsg)
+  {
+    ezMsg::ezBigMsg* big=msg->heap_.ptr_;
+    if(!(msg->flags_&shared) || !big->refcnt_.Sub(1)) 
+    {
+      big->refcnt_.~AtomicNumber();
+      if(big->ffn_)
+        big->ffn_(big->data_,big->hint_);
+      free (big);
+    }
+  }
+  msg->type_=0;
+}
+
+void net::ezMsgCopy(ezMsg* src,ezMsg* dst)
+{  
+  ezMsgFree(dst);
+  if(src->type_==type_lmsg)
+  {
+    if(src->flags_&shared)
+      src->heap_.ptr_->refcnt_.Add(1);
+    else
+      {
+        src->heap_.ptr_->refcnt_.Set(2);
+        src->flags_|=shared;
+    }
+  }
+  *dst=*src;
 }
--- net/netpack.h
+++ net/netpack.h
@@ -1,29 +1,69 @@
 #ifndef _NETPACK_H
 #define _NETPACK_H
 #include "portable.h"
-#include "../base/list.h"
+#include "../base/thread.h"
 
+// from 0mq
+extern "C"
+{
+  typedef void (msg_free_fn) (void *data, void *hint);
+}
 namespace net
 {
-	struct ezNetPack 
-	{
-		uint16_t capacity_;
-		uint16_t size_;
-		char* data_;
+  enum
+  {
+    more = 1,
+    identity = 64,
+    shared = 128
+  };
+  enum {max_vsm_size = 60};
+  enum type_t
+  {
+    type_min = 101,
+    type_vsm = 101,
+    type_lmsg = 102,
+    type_delimiter = 103,
+    type_max = 103
+  };
+  struct ezMsg
+  {
+    struct ezBigMsg
+    {
+      int8_t*          data_;
+      uint16_t         capcity_;
+      msg_free_fn*     ffn_;
+      void*            hint_;
+      base::AtomicNumber refcnt_;
+    };
+    uint16_t size_;
+    int8_t   type_;
+    int8_t   flags_;
+    union
+    {
+      struct
+      {
+        int8_t data_[max_vsm_size];
+      }stack_;
+      struct
+      {
+        ezBigMsg* ptr_;
+      }heap_;
+    };
+  };
 
-		explicit ezNetPack(uint16_t s);
-		virtual ~ezNetPack();
-	};
+  void ezMsgInit(ezMsg* msg);
+  void ezMsgInitSize(ezMsg* msg,int size);
+  void ezMsgInitData(ezMsg* msg,int8_t* data,int size,msg_free_fn* ffn,void* hint);
+  void ezMsgInitDelimiter(ezMsg* msg);
 
-	struct ezSendBlock
-	{
-		list_head lst_;
-		int fd_;
-		ezNetPack* pack_;
+  int  ezMsgSize(ezMsg* msg);
+  int  ezMsgCapcity(ezMsg* msg);
+  int8_t* ezMsgData(ezMsg* msg);
 
-		ezSendBlock();
-		virtual ~ezSendBlock();
-	};
+  void ezMsgCopy(ezMsg* src,ezMsg* dst);
+  void ezMsgAddRef(ezMsg* msg,int ref);
+  void ezMsgSubRef(ezMsg* msg,int ref);
+  void ezMsgFree(ezMsg* msg);
 }
 
 #endif
